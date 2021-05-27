69a7abb1ad57c5efb57cd5364d1f7b40495d5a57
Andrea <54257897+AndreaFZP@users.noreply.github.com>
2021-05-26 18:22:34 -0600

    Classes added, tools and main started

---
+++ MEF2D/MEF2D/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MEF2D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string filename = new string(new char[150]);
+			filename = args[1];
+
+			List<List<List<float>>> localKs = new List<List<List<float>>>();
+			List<List<float>> localbs = new List<List<float>>();
+			List<List<float>> K = new List<List<float>>();
+			List<float> b = new List<float>();
+			List<float> T = new List<float>();
+
+			Console.Write("IMPLEMENTACION DEL METODO DE LOS ELEMENTOS FINITOS\n");
+			Console.Write("\t- TRANSFERENCIA DE CALOR\n");
+			Console.Write("\t- 2 DIMENSIONES\n");
+			Console.Write("\t- FUNCIONES DE FORMA LINEALES\n");
+			Console.Write("\t- PESOS DE GALERKIN\n");
+			Console.Write("\t- MALLA TRIANGULAR IRREGULAR\n");
+			Console.Write("*********************************************************************************\n\n");
+		}
+	}
+}
---
+++ MEF2D/MEF2D/Tools.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace MEF2D
+{
+    class tools
+    {
+
+	}
+}
